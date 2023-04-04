23e5fd5581fa8efc3ccbdc12a20dda1e7bb018be
Martí Arnau <marnauortega@gmail.com>
2023-04-04 22:19:46 +0200

    feat: add back button

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,10 +1,17 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./Logo.module.css";
+import { usePathname, useRouter } from "next/navigation";
+import { ChevronLeft } from "react-feather";
 
 const Logo = () => {
+  const router = useRouter();
+  const pathName = usePathname();
+
   return (
     <Link href="/">
-      <div className={styles.logo}></div>
+      <div className={styles.logo}>{pathName === "/" ? "" : <ChevronLeft color="white" className={styles.icon} />}</div>
     </Link>
   );
 };
