2cf86c60e8f9bc7d8757b0e5b4175fa0aecf1241
Martí Arnau <marnauortega@gmail.com>
2023-04-07 13:37:51 +0200

    fix: optional chaining for portable text and chart

--- app/(website)/[slug]/page.js
+++ app/(website)/[slug]/page.js
@@ -14,17 +14,18 @@ export const generateStaticParams = async () => {
 
 const WorkPage = async ({ params }) => {
   const [{ title, chartToggled, chart, content }] = await getWork(params.slug);
-
   // Text is short if it has less than three paragraphs and none of them are longer than 500 characters
   const textIsShort =
-    content.length <= 3 && content.filter((paragraph) => paragraph.children[0].text.length > 500).length === 0;
+    content?.length <= 3 &&
+    content?.filter((paragraph) => paragraph._type === "block" && paragraph?.children[0]?.text?.length > 500).length ===
+      0;
 
   return (
     <div className={styles.content}>
       <h1 className={styles.heading}>{title}</h1>
       {chartToggled && (
         <dl className={`${styles.chart} ${textIsShort ? styles.chartInsideLayout : ""}`}>
-          {chart.map(({ title, content }) => (
+          {chart?.map(({ title, content }) => (
             <Fragment key={title}>
               <dt className={styles.chartTitle}>{title}</dt>
               <dd className={styles.chartContent}>{content}</dd>
