26fe04bbcaa09ee8de1f818eb806fc069fef243c
Martí Arnau <marnauortega@gmail.com>
2023-04-10 11:53:58 +0200

    feat: add cdn to sanity

--- sanity/clientConfig.js
+++ sanity/clientConfig.js
@@ -1,1 +1,7 @@
-export default { projectId: "jpoqf5n3", dataset: "production", useCdn: false, apiVersion: "2023-03-04" };
+export default {
+  projectId: "jpoqf5n3",
+  dataset: "production",
+  useCdn: true,
+  apiVersion: "2023-03-04",
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+};
